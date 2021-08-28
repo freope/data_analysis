cd63fa09ff4e30199f002f20a0b4c8317bcd7fb9
vagrant <vagrant@vagrant.vm>
2021-08-28 19:53:37 +0000

    集合クラス

---
+++ frestu/optimization/ga/population.py
@@ -0,0 +1,35 @@
+class Population:
+    
+    def __init__(self, individuals):
+        self.individuals = None
+        self.next_individuals = None
+    
+    def alternate(self):
+        # エリート保存戦略
+
+        # 親を選択
+
+        # 交叉
+
+        # 突然変異
+
+        # 世代交代
+
+        # 評価
+
+        pass
+
+    def can_alternate(self):
+        pass
+
+    def print_result(self):
+        pass
+    
+    def __evaluate(self):
+        pass
+    
+    def __select(self):
+        pass
+    
+    def __sort(self):
+        pass
