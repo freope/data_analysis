7a1df68aca707e3b6e3123b310c46420c86ab6bc
vagrant <vagrant@vagrant.vm>
2021-08-29 05:12:17 +0000

    fitness 属性をプライベート属性に変更

--- frestu/optimization/ga/individual.py
+++ frestu/optimization/ga/individual.py
@@ -3,19 +3,23 @@ import copy
 
 class Individual:
     
-    def __init__(self, chromosome, evaluator, learning_rate=0.5):
+    def __init__(self, chromosome, evaluator, learning_rate=1.5):
         self.chromosome = chromosome
         self.evaluator = evaluator
-        self.fitness = None
         self.learning_rate = learning_rate
-        
+        self.__fitness = None
+
+    @property
+    def fitness(self):
+        return self.__fitness    
+
     def crossover(self, parent_female, parent_male):
         for gene_name, gene_female in parent_female.chromosome.items():
             gene_male = parent_male.chromosome[gene_name]
             self.chromosome[gene_name] = gene_female.crossover(
                 gene_male,
-                fitness_self=self.parent_female.fitness,
-                fitness_partner=self.parent_male.fitness,
+                fitness_self=parent_female.fitness,
+                fitness_partner=parent_male.fitness,
                 learning_rate=self.learning_rate)
     
     def mutate(self):
@@ -23,15 +27,15 @@ class Individual:
             gene.mutate()
 
     def evaluate(self):
-        self.fitness = self.evaluator(self.chromosome)
+        self.__fitness = self.evaluator(self.chromosome)
 
     def save(self):
         pass
     
     @classmethod
-    def create(cls, chromosome_prototype, evaluator):
+    def create(cls, chromosome_prototype, evaluator, learning_rate=1.5):
         chromosome = copy.deepcopy(chromosome_prototype)
         # 各 gene の values に値を設定する
         for gene in chromosome.values():
             gene.realize()
-        return cls(chromosome, evaluator)
+        return cls(chromosome, evaluator, learning_rate)
