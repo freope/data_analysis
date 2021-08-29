96704c94c4274835af475f3a98b59c82499a757f
vagrant <vagrant@vagrant.vm>
2021-08-29 03:13:03 +0000

    n 点交叉を実装

--- frestu/optimization/ga/crossover.py
+++ frestu/optimization/ga/crossover.py
@@ -1,5 +1,38 @@
-def crossover_n_point():
-    pass
+import copy
+import random
+
+import numpy as np
+
+
+def crossover_n_point(vec1, vec2, n_point):
+    size = min(vec1.shape[0], vec2.shape[0])
+    max_n = size - 1
+    
+    if max_n < n_point:
+        raise ValueError(f'n_point should be {max_n} or less.')
+    
+    separating_positions = sorted(random.sample(range(1, size), k=n_point))
+    separating_positions.insert(0, 0)
+    separating_positions.append(size)
+        
+    booleans =[True, False]
+    # vec1 と vec2 の順序の区別を無くす
+    random.shuffle(booleans)
+    
+    exchanging_positions = np.full(size, booleans[0])
+    exchanging_times = len(separating_positions) // 2
+
+    for i in range(exchanging_times):
+        ix = i * 2
+        start = separating_positions[ix]
+        stop = separating_positions[ix+1]
+        exchanging_positions[start:stop] = booleans[1]
+    
+    vec = copy.copy(vec1)
+    vec[exchanging_positions] = vec2[exchanging_positions]
+    
+    return vec
+
 
 def crossover_uniform():
     pass
--- frestu/tests/optimization/ga/crossover_tests.py
+++ frestu/tests/optimization/ga/crossover_tests.py
@@ -9,13 +9,18 @@ from frestu.optimization.ga.crossover import *
 class CrossoverTests(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.vec1 = np.array([1, 2, 3, 4, 5])
+        self.vec2 = np.array([6, 7, 8, 9, 0])
     
     def tearDown(self):
         pass
 
     def test_crossover_n_point(self):
-        crossover_n_point()
+        vec = crossover_n_point(self.vec1, self.vec2, n_point=1)
+        # HACK: 
+        # 遺伝子が交換されていることをテストし、交換のされ方はテストできていない。
+        self.assertEqual(list(vec == self.vec1), list(vec != self.vec2))
+        self.assertEqual(list(vec == self.vec2), list(vec != self.vec1))
 
 
 if __name__ == '__main__':
