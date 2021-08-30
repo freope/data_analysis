1c377f94559f2fc48c659ffdfba96a4219f22aa7
vagrant <vagrant@vagrant.vm>
2021-08-30 03:33:57 +0000

    テストスイートを追加

--- frestu/tests/tests.py
+++ frestu/tests/tests.py
@@ -8,8 +8,10 @@ from frestu.tests.preprocessing.data_frame_fx_tests import DataFrameFxTests
 from frestu.tests.optimization.ga.gene.gene_discrete_tests import GeneDiscreteTests
 from frestu.tests.optimization.ga.gene.gene_continuous_tests import GeneContinuousTests
 from frestu.tests.optimization.ga.gene.gene_continuous_logscale_tests import GeneContinuousLogscaleTests
+from frestu.tests.optimization.ga.population_tests import PopulationTests
 from frestu.tests.optimization.ga.individual_tests import IndividualTests
 from frestu.tests.optimization.ga.crossover_tests import CrossoverTests
+from frestu.tests.optimization.ga.selection_tests import SelectionTests
 
 
 def test_suite():
@@ -22,8 +24,10 @@ def test_suite():
     suite.addTests(unittest.makeSuite(GeneDiscreteTests))
     suite.addTests(unittest.makeSuite(GeneContinuousTests))
     suite.addTests(unittest.makeSuite(GeneContinuousLogscaleTests))
+    suite.addTests(unittest.makeSuite(PopulationTests))
     suite.addTests(unittest.makeSuite(IndividualTests))
     suite.addTests(unittest.makeSuite(CrossoverTests))
+    suite.addTests(unittest.makeSuite(SelectionTests))
 
     return suite
 
